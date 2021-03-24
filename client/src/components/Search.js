d6a40b48819d3dfa37aea05bf1d970deef0e55e8
Vekisis <damianburbo@yahoo.com>
2021-03-24 16:35:10 -0400

    Added an incomplete search function

---
+++ client/src/components/Search.js
@@ -0,0 +1,13 @@
+//Starter
+
+export default function search(){
+
+
+    return(
+        <form onClick="/search" method="GET">
+            <input type="search" name=""/>
+            <button type="submit">Search</button>
+
+        </form>
+    )
+}
