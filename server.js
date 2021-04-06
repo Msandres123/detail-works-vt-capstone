3fa398cb383cd70f50eb0d6c528953efc66c941f
Mikael Andres <mikaelandres@Mikaels-MacBook-Pro.local>
2021-04-06 16:16:35 -0400

    atlasdb

--- server.js
+++ server.js
@@ -27,8 +27,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("./client/public"));
 app.use("/static", express.static(path.join(__dirname, "public")));
 
+const MONGODB_URI = process.env.MONGODB_URI
+
 //set-up to the database(local)
-mongoose.connect("mongodb://localhost:27017/schedule", {
+mongoose.connect(MONGODB_URI || "mongodb://localhost:27017/schedule", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
