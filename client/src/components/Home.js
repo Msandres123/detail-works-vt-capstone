42131dc93308e69f284fb277c5b967ba55d338fd
Mikael Andres <mikaelandres@Mikaels-MacBook-Pro.local>
2021-05-13 11:19:14 -0400

    no Covid

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -9,30 +9,6 @@ export default function Home() {
     <>
       <div className="home-container">
         <img id="logo" src={Logo} width="225" alt="Detail Works Logo" title="Detail Works Logo"/>
-        
-        <h2>Detail Works Appointment Scheduler</h2>
-        <p>
-          Please book your appointment with Detail Works, a Spectrum Enterprise,
-          using the below form. We are located at 22 Avenue C, Williston, VT
-          05495 in a large blue building on the corner of Avenue C and
-          Industrial Avenue.
-        </p>
-        <h3>
-          PLEASE NOTE OUR DROP OFF AND PICK UP PROCEDURE HAS CHANGED DUE TO
-          COVID-19.
-        </h3>
-        <p>
-          Please drop off your vehicle at 22 Avenue C, Williston VT and leave
-          your keys in the overnight drop box located at the front of our
-          building facing Avenue C. Once you’ve dropped your vehicle off call,
-          text, or email us and we will retrieve your car for service. Once your
-          vehicle is completed we will call you for payment and with pickup
-          instructions. Early drop-offs are welcome and appreciated.
-        </p>
-        <p>
-          Comments, questions, or concerns? Give us a call! Our number is (802)
-          497-2296.
-        </p>
       </div>
       <AppointmentScheduler />
     </>
